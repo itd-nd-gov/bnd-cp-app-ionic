0a39fa75cf25b5b110019b83b475a3679bc94564
Elijah Cornell <elijah.cornell@gmail.com>
2014-09-17 11:43:13 -0500

    data service - switched to resolver

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,60 +1,52 @@
+"use strict";
+
 angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function($scope) {})
+.controller('MainCtrl', function ($scope) {})
 
-.controller('CalendarCtrl', function($scope, $window, $http, $filter, $ionicLoading, Data) {
+.controller('CalendarCtrl', function ($scope, $window, $http, $filter, $ionicLoading, Data, events) {
 
     $ionicLoading.show({
-  	    content: 'Loading Data',
-  	    animation: 'fade-in',
-  	    showBackdrop: false,
-  	    maxWidth: 200,
-  	    showDelay: 250
-  	});
+        content: 'Loading Data',
+        animation: 'fade-in',
+        showBackdrop: false,
+        maxWidth: 200,
+        showDelay: 250
+    });
 
     //
 
-    Data.readGoogleCalendar().then( function( events ) {
+    var entry;
+    var results = [];
+    var curDate;
+    var prevDate;
+    var dateGroup = [];
 
-        //Data.setEvents(events);
+    for (var k in events.feed.entry) {
 
-        var entry;
-        var results = [];
-        var curDate;
-        var prevDate;
-        var dateGroup = [];
+        entry = events.feed.entry[k];
 
-        for (var k in events.feed.entry) {
+        curDate = entry.gd$when[0].startTime.substring(0, 10);
 
-            entry = events.feed.entry[k];
-
-            curDate = entry.gd$when[0].startTime.substring(0,10);
-
-            if (curDate != prevDate) {
-              dateGroup = [curDate, [entry]];
-              results.push( dateGroup );
-              prevDate = curDate;
-            } else {
-              dateGroup[1].push( entry );
-            }
-
+        if (curDate != prevDate) {
+            dateGroup = [curDate, [entry]];
+            results.push(dateGroup);
+            prevDate = curDate;
+        } else {
+            dateGroup[1].push(entry);
         }
 
-        $scope.results = results;
+    }
 
-        console.log(results);
+    $scope.results = results;
 
-        $ionicLoading.hide();
-
-      }
-
-  );
+    $ionicLoading.hide();
 
 })
 
 
-.controller('CalendarDetailCtrl', function($scope, $window, $http, $filter, $ionicLoading, Data, $stateParams) {
+.controller('CalendarDetailCtrl', function ($scope, $window, $http, $filter, $ionicLoading, Data, $stateParams) {
 
-  $scope.entry = Data.getEventDetails($stateParams.id);
+    $scope.entry = Data.getEventDetails($stateParams.id);
 
 });
