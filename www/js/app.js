5bf6df5f394d202864c69befd511356caa6197cc
Elijah Cornell <elijah.cornell@gmail.com>
2014-09-18 15:13:11 -0500

    hide time on all day events source formatting cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,57 +1,58 @@
-"use strict";
+(function() {
+  "use strict";
 
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+  var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        // if(window.cordova && window.cordova.plugins.Keyboard) {
-        //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        // }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      // if(window.cordova && window.cordova.plugins.Keyboard) {
+      //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      // }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
     });
-})
+  })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
 
     $stateProvider
 
     .state('main', {
-        url: '/main',
-        templateUrl: 'templates/main.html',
-        controller: 'MainCtrl'
+      url: '/main',
+      templateUrl: 'templates/main.html',
+      controller: 'MainCtrl'
     })
 
     .state('about', {
-        url: '/about',
-        templateUrl: 'templates/about.html',
+      url: '/about',
+      templateUrl: 'templates/about.html',
     })
 
     .state('calendar', {
-        url: '/calendar',
-        templateUrl: 'templates/calendar.html',
-        controller: 'CalendarCtrl',
-        resolve: {
-            events: function (Data) {
-                return Data.readGoogleCalendar()
-            }
+      url: '/calendar',
+      templateUrl: 'templates/calendar.html',
+      controller: 'CalendarCtrl',
+      resolve: {
+        events: function(Data) {
+          return Data.readGoogleCalendar();
         }
+      }
     })
 
     .state('calendar-detail', {
-        url: '/detail/:id',
-        templateUrl: 'templates/calendar-detail.html',
-        controller: 'CalendarDetailCtrl'
+      url: '/detail/:id',
+      templateUrl: 'templates/calendar-detail.html',
+      controller: 'CalendarDetailCtrl'
     })
 
     .state('contact', {
-        url: '/contact',
-        templateUrl: 'templates/contact.html',
+      url: '/contact',
+      templateUrl: 'templates/contact.html',
     })
 
     ;
@@ -59,4 +60,6 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.se
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/main');
 
-});
+  });
+
+})();
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,18 @@
-"use strict";
+(function() {
+  "use strict";
 
-angular.module('starter.controllers', [])
+  angular.module('starter.controllers', [])
 
-.controller('MainCtrl', function ($scope) {})
+  .controller('MainCtrl', function($scope) {})
 
-.controller('CalendarCtrl', function ($scope, $window, $http, $filter, $ionicLoading, Data, events) {
+  .controller('CalendarCtrl', function($scope, $window, $http, $filter, $ionicLoading, Data, events) {
 
     $ionicLoading.show({
-        content: 'Loading Data',
-        animation: 'fade-in',
-        showBackdrop: false,
-        maxWidth: 200,
-        showDelay: 250
+      content: 'Loading Data',
+      animation: 'fade-in',
+      showBackdrop: false,
+      maxWidth: 200,
+      showDelay: 250
     });
 
     //
@@ -24,17 +25,17 @@ angular.module('starter.controllers', [])
 
     for (var k in events.feed.entry) {
 
-        entry = events.feed.entry[k];
+      entry = events.feed.entry[k];
 
-        curDate = entry.gd$when[0].startTime.substring(0, 10);
+      curDate = entry.gd$when[0].startTime.substring(0, 10);
 
-        if (curDate != prevDate) {
-            dateGroup = [curDate, [entry]];
-            results.push(dateGroup);
-            prevDate = curDate;
-        } else {
-            dateGroup[1].push(entry);
-        }
+      if (curDate != prevDate) {
+        dateGroup = [curDate, [entry]];
+        results.push(dateGroup);
+        prevDate = curDate;
+      } else {
+        dateGroup[1].push(entry);
+      }
 
     }
 
@@ -42,11 +43,13 @@ angular.module('starter.controllers', [])
 
     $ionicLoading.hide();
 
-})
+  })
 
 
-.controller('CalendarDetailCtrl', function ($scope, $window, $http, $filter, $ionicLoading, Data, $stateParams) {
+  .controller('CalendarDetailCtrl', function($scope, $window, $http, $filter, $ionicLoading, Data, $stateParams) {
 
     $scope.entry = Data.getEventDetails($stateParams.id);
 
-});
+  });
+
+})();
